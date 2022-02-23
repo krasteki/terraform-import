a04e7d1272e3230f2b79a4461edf440890b53d6b
krasteki <kiril.krastev@hashicorp.com>
2022-02-23 11:18:59 +0200

    my_first_commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+# Configure docker provider
+#
+# https://www.terraform.io/docs/providers/docker/index.html
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+
